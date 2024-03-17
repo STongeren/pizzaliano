8373c00e752676da149bd21abe23936eb4b6fba9
STongeren <302563303@student.rocmondriaan.nl>
2024-03-17 14:36:45 +0100

    added that you see the name whenever you click on a category.

--- src/Controller/CategoriesShow.php
+++ src/Controller/CategoriesShow.php
@@ -1,18 +1,41 @@
 <?php
-// src/Controller/CategoriesShowController.php
+// src/Controller/CategoriesShow.php
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesShow extends AbstractController
 {
+    private $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
     #[Route('/category/show/{id}', name: 'category_show')]
     public function show(int $id): Response
     {
-        // Redirect to categories show base page with category ID as a parameter
-        return $this->render('pages/categoriesshow.html.twig', ['id' => $id]);
+        // Retrieve the category object by ID
+        $category = $this->categoryRepository->find($id);
+
+        // If category not found, return a 404 response
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        // Debugging: Dump category object to see its properties
+        dump($category);
+
+        // Render the template and pass both ID and name as variables
+        return $this->render('pages/categoriesshow.html.twig', [
+            'id' => $id,
+            'name' => $category->getName(), // Assuming the name property exists in Category entity
+        ]);
     }
 }
--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -8,11 +8,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Category>
- *
- * @method Category|null find($id, $lockMode = null, $lockVersion = null)
- * @method Category|null findOneBy(array $criteria, array $orderBy = null)
- * @method Category[]    findAll()
- * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class CategoryRepository extends ServiceEntityRepository
 {
@@ -21,28 +16,33 @@ class CategoryRepository extends ServiceEntityRepository
         parent::__construct($registry, Category::class);
     }
 
-//    /**
-//     * @return Category[] Returns an array of Category objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Find categories by name.
+     *
+     * @param string $name The name of the category
+     * @return Category[] Returns an array of Category objects
+     */
+    public function findByName(string $name): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.name = :name')
+            ->setParameter('name', $name)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Category
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * Find one category by name.
+     *
+     * @param string $name The name of the category
+     * @return Category|null Returns a Category object or null if not found
+     */
+    public function findOneByName(string $name): ?Category
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.name = :name')
+            ->setParameter('name', $name)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
