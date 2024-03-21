c5e7b41fd8c30bd389e79dec61c2ba72b250a7e9
STongeren <302563303@student.rocmondriaan.nl>
2024-03-21 23:06:35 +0100

    DB added

--- sopranos (2).sql
+++ sopranos (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 15 mrt 2024 om 20:33
+-- Gegenereerd op: 21 mrt 2024 om 23:05
 -- Serverversie: 10.4.28-MariaDB
 -- PHP-versie: 8.2.4
 
@@ -28,17 +28,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `category`
 --
 
-INSERT INTO `category` (`id`) VALUES
-(1),
-(2),
-(3);
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Vegetarian'),
+(2, 'Meat');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,15 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20240214135409', '2024-02-14 14:54:19', 34),
 ('DoctrineMigrations\\Version20240214141840', '2024-02-14 15:18:47', 25),
 ('DoctrineMigrations\\Version20240214142202', '2024-02-14 15:22:18', 33),
-('DoctrineMigrations\\Version20240214142430', '2024-02-14 15:24:36', 16);
+('DoctrineMigrations\\Version20240214142430', '2024-02-14 15:24:36', 16),
+('DoctrineMigrations\\Version20240317131117', '2024-03-17 14:11:34', 14),
+('DoctrineMigrations\\Version20240317131708', '2024-03-17 14:17:16', 7),
+('DoctrineMigrations\\Version20240317134311', '2024-03-17 14:43:15', 8),
+('DoctrineMigrations\\Version20240317134501', '2024-03-17 14:48:05', 5),
+('DoctrineMigrations\\Version20240317135110', '2024-03-17 14:52:45', 2),
+('DoctrineMigrations\\Version20240317140000', '2024-03-17 15:17:13', 15),
+('DoctrineMigrations\\Version20240317142520', '2024-03-17 15:26:24', 3),
+('DoctrineMigrations\\Version20240317144216', '2024-03-17 16:40:13', 24);
 
 -- --------------------------------------------------------
 
@@ -94,24 +102,25 @@ CREATE TABLE `messenger_messages` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `pizzas`
+-- Tabelstructuur voor tabel `pizza`
 --
 
-CREATE TABLE `pizzas` (
+CREATE TABLE `pizza` (
   `id` int(11) NOT NULL,
   `name` longtext NOT NULL,
-  `price` decimal(4,2) NOT NULL
+  `price` decimal(4,2) NOT NULL,
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Gegevens worden geëxporteerd voor tabel `pizzas`
+-- Gegevens worden geëxporteerd voor tabel `pizza`
 --
 
-INSERT INTO `pizzas` (`id`, `name`, `price`) VALUES
-(1, 'Pizza de la margeritha', 7.00),
-(2, 'Mozzarella Pizza', 33.00),
-(3, 'Pizza Hot Chicken', 12.00),
-(4, 'The Vegan California', 13.50);
+INSERT INTO `pizza` (`id`, `name`, `price`, `category_id`) VALUES
+(1, 'Pizza de la Margeritha', 7.00, 1),
+(2, 'Mozzarella Pizza', 33.00, 1),
+(3, 'Pizza Hot Chicken', 12.00, 2),
+(4, 'Chicken California Pizza', 13.50, 2);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -145,10 +154,11 @@ ALTER TABLE `messenger_messages`
   ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
 
 --
--- Indexen voor tabel `pizzas`
+-- Indexen voor tabel `pizza`
 --
-ALTER TABLE `pizzas`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pizza_category` (`category_id`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -173,10 +183,20 @@ ALTER TABLE `messenger_messages`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `pizzas`
+-- AUTO_INCREMENT voor een tabel `pizza`
 --
-ALTER TABLE `pizzas`
+ALTER TABLE `pizza`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `pizza`
+--
+ALTER TABLE `pizza`
+  ADD CONSTRAINT `fk_pizza_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
