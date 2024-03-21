171f91e844336b8808ff858ef6832664f9df8a4e
STongeren <302563303@student.rocmondriaan.nl>
2024-03-21 19:39:07 +0100

    Made card for pizzas

--- src/Controller/CategoriesShow.php
+++ src/Controller/CategoriesShow.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Pizza;
 use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,12 +15,12 @@ class CategoriesShow extends AbstractController
     private $pizzaRepository;
 
     public function __construct(PizzaRepository $pizzaRepository)
-    {
+    { 
         $this->pizzaRepository = $pizzaRepository;
     }
 
     #[Route('/category/show/{id}', name: 'category_show')]
-    public function show(int $id): Response
+    public function show(int $id, Request $request): Response
     {
         // Retrieve the pizzas belonging to the category with the given ID
         $pizzas = $this->pizzaRepository->findBy(['category_id' => $id]);
@@ -27,6 +28,34 @@ class CategoriesShow extends AbstractController
         // If no pizzas found, return a 404 response
         if (empty($pizzas)) {
             throw $this->createNotFoundException('Pizzas not found for this category');
+        }
+
+        // Handle adding pizza to cart if requested
+        if ($request->isMethod('POST') && $request->request->has('pizza_id')) {
+            $pizzaId = $request->request->getInt('pizza_id');
+            $pizza = $this->pizzaRepository->find($pizzaId);
+
+            if ($pizza) {
+                // Get or initialize the shopping cart in session
+                $cart = $request->getSession()->get('cart', []);
+
+                // Add pizza to the cart
+                if (!isset($cart[$pizzaId])) {
+                    $cart[$pizzaId] = [
+                        'name' => $pizza->getName(),
+                        'price' => $pizza->getPrice(),
+                        'quantity' => 1,
+                    ];
+                } else {
+                    $cart[$pizzaId]['quantity']++;
+                }
+
+                // Store the updated cart back in session
+                $request->getSession()->set('cart', $cart);
+
+                $this->addFlash('success', 'Pizza added to cart.');
+                return $this->redirectToRoute('category_show', ['id' => $id]);
+            }
         }
 
         // Render the template and pass pizzas as variables
