cb4f15b04d8bbf9076625ef2fc2baafa42e5b33e
STongeren <302563303@student.rocmondriaan.nl>
2024-03-21 23:05:36 +0100

    You can now delete stuff from the order

--- src/Controller/CategoriesShow.php
+++ src/Controller/CategoriesShow.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/Controller/CategoriesShow.php
 
 namespace App\Controller;
@@ -15,8 +16,23 @@ class CategoriesShow extends AbstractController
     private $pizzaRepository;
 
     public function __construct(PizzaRepository $pizzaRepository)
-    { 
+    {
         $this->pizzaRepository = $pizzaRepository;
+    }
+
+    #[Route('/cart/remove', name: 'remove_from_cart', methods: ['POST'])]
+    public function removeFromCart(Request $request): Response
+    {
+        $itemId = $request->request->getInt('item_id');
+        $cart = $request->getSession()->get('cart', []);
+
+        if (isset($cart[$itemId])) {
+            unset($cart[$itemId]);
+            $request->getSession()->set('cart', $cart);
+        }
+
+        // Redirect back to the same page after removing item from cart
+        return $this->redirectToRoute('category_show', ['id' => $request->query->getInt('category_id')]);
     }
 
     #[Route('/category/show/{id}', name: 'category_show')]
@@ -24,43 +40,47 @@ class CategoriesShow extends AbstractController
     {
         // Retrieve the pizzas belonging to the category with the given ID
         $pizzas = $this->pizzaRepository->findBy(['category_id' => $id]);
-
+    
         // If no pizzas found, return a 404 response
         if (empty($pizzas)) {
             throw $this->createNotFoundException('Pizzas not found for this category');
         }
-
+    
         // Handle adding pizza to cart if requested
         if ($request->isMethod('POST') && $request->request->has('pizza_id')) {
             $pizzaId = $request->request->getInt('pizza_id');
             $pizza = $this->pizzaRepository->find($pizzaId);
-
+    
             if ($pizza) {
-                // Get or initialize the shopping cart in session
+                // Get the cart from session or initialize it if not exists
                 $cart = $request->getSession()->get('cart', []);
-
-                // Add pizza to the cart
-                if (!isset($cart[$pizzaId])) {
+    
+                // Add pizza to cart
+                if (isset($cart[$pizzaId])) {
+                    $cart[$pizzaId]['quantity']++;
+                } else {
                     $cart[$pizzaId] = [
                         'name' => $pizza->getName(),
                         'price' => $pizza->getPrice(),
                         'quantity' => 1,
                     ];
-                } else {
-                    $cart[$pizzaId]['quantity']++;
                 }
-
-                // Store the updated cart back in session
+    
+                // Store cart in session
                 $request->getSession()->set('cart', $cart);
-
-                $this->addFlash('success', 'Pizza added to cart.');
-                return $this->redirectToRoute('category_show', ['id' => $id]);
             }
+    
+            // Redirect back to the same page to prevent form resubmission
+            return $this->redirectToRoute('category_show', ['id' => $id]);
         }
-
-        // Render the template and pass pizzas as variables
+    
+        // Retrieve cart items from session
+        $cartItems = $request->getSession()->get('cart', []);
+    
         return $this->render('pages/categoriesshow.html.twig', [
+            'id' => $id, // Pass the category ID to the template
             'pizzas' => $pizzas,
+            'cartItems' => $cartItems,
         ]);
     }
 }
