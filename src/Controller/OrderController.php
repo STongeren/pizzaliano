377caa63c653206f6b476604cdaa78a3b7079831
STongeren <302563303@student.rocmondriaan.nl>
2024-03-23 01:57:46 +0100

    Added page after you order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,18 +3,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order/submit', name: 'submit_order')]
-    public function submitOrder(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        // Handle order submission logic here
-        
-        // Example: Return a simple response
-        return new Response('Order submitted successfully');
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/order', name: 'submit_order')]
+    public function submitOrder(Request $request): Response
+    {
+        $order = new Order();
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($order);
+            $this->entityManager->flush();
+
+            // Optionally, redirect to a success page
+            return $this->redirectToRoute('order_success');
+        }
+
+        return $this->render('/pages/order/submit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Entity/Order.php
@@ -0,0 +1,60 @@
+<?php
+// src/Entity/Order.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OrderRepository")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    // Add more properties and methods as needed...
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
